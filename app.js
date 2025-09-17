8820716fadcebaba3e99b1267eb9a39020163dd7
Macbook <macbook@MacBook-Air-Aqgul.local>
2025-09-17 14:45:16 +0500

    Add frontend

--- app.js
+++ app.js
@@ -60,10 +60,18 @@ letterFilter.addEventListener("change", () => {
 categoryButtons.forEach((btn) => {
   btn.addEventListener("click", () => {
 
-    categoryButtons.forEach((b) => b.classList.remove("bg-indigo-600","text-white"));
-    categoryButtons.forEach((b) => b.classList.add("bg-gray-200","text-gray-700"));
-    btn.classList.remove("bg-gray-200","text-gray-700");
-    btn.classList.add("bg-indigo-600","text-white");
+  
+    categoryButtons.forEach((b) => {
+      b.classList.remove("text-white");
+      b.style.backgroundColor = "#E5E7EB"; 
+      b.classList.add("text-gray-700");
+    });
+
+
+    btn.classList.remove("text-gray-700");
+    btn.classList.add("text-white");
+    btn.style.backgroundColor = "#745EB6"; 
+
 
     const category = btn.dataset.category;
     if (category === "all") {
@@ -73,8 +81,74 @@ categoryButtons.forEach((btn) => {
         (e) => e.category.toLowerCase() === category.toLowerCase()
       );
     }
+
     renderEmojis(filteredEmojis);
   });
 });
 
+
 fetchEmojis();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function renderEmojis(emojis) {
+  const container = document.getElementById("emojiList");
+  container.innerHTML = "";
+
+  let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+  emojis.forEach((emoji) => {
+    const card = document.createElement("div");
+    card.className =
+      "relative shadow-lg rounded-2xl p-6 flex flex-col items-center justify-center hover:scale-105 transform transition duration-300";
+
+
+    card.innerHTML = `
+      <div class="text-5xl mb-3">${emoji.htmlCode[0]}</div>
+      <p class="font-bold text-gray-800 text-center mb-1">${emoji.name}</p>
+      <p class="text-sm text-indigo-600 italic mb-2">${emoji.category}</p>
+      <button class="favorite-btn absolute top-2 right-2">
+        <svg xmlns="http://www.w3.org/2000/svg" 
+             class="h-6 w-6 ${favorites.some(fav => fav.name === emoji.name) ? 'text-red-500' : 'text-gray-400'}" 
+             fill="currentColor" viewBox="0 0 24 24">
+          <path d="M12 21.35l-1.45-1.32C5.4 15.36 
+                   2 12.28 2 8.5 
+                   2 5.42 4.42 3 7.5 3 
+                   c1.74 0 3.41 0.81 4.5 2.09 
+                   C13.09 3.81 14.76 3 16.5 3 
+                   19.58 3 22 5.42 22 8.5 
+                   c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
+        </svg>
+      </button>
+    `;
+
+    const favBtn = card.querySelector(".favorite-btn");
+    favBtn.addEventListener("click", () => {
+      let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+      const exists = favorites.some((f) => f.name === emoji.name);
+
+      if (exists) {
+        favorites = favorites.filter((f) => f.name !== emoji.name);
+      } else {
+        favorites.push(emoji);
+      }
+
+      localStorage.setItem("favorites", JSON.stringify(favorites));
+      renderEmojis(emojis); 
+    });
+
+    container.appendChild(card);
+  });
+}
