693d7b68579c5f8cd89e490fab3ef1b469020da4
Macbook <macbook@MacBook-Air-Aqgul.local>
2025-09-17 15:37:13 +0500

    Fix: add process.env.PORT for Railway

--- server.js
+++ server.js
@@ -17,5 +17,5 @@ app.get("/api/emojis", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
